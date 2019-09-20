4dcb5158aefd008a8be2f16f510d36738d664783
tuannguyen1299 <tuannguyennst99@gmail.com>
2019-09-20 15:24:25 +0700

    chap11

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,12 +13,24 @@ module SessionsHelper
   end
 
   # Returns the current logged-in user (if any).
+  # def current_user
+  #   if (user_id = session[:user_id])
+  #     @current_user ||= User.find_by(id: user_id)
+  #   elsif (user_id = cookies.signed[:user_id])
+  #     user = User.find_by(id: user_id)
+  #     if user && user.authenticated?(cookies[:remember_token])
+  #       log_in user
+  #       @current_user = user
+  #     end
+  #   end
+  # end
+  
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
+      if user && user.authenticated?(:remember, cookies[:remember_token])
         log_in user
         @current_user = user
       end
@@ -32,15 +44,15 @@ module SessionsHelper
 
   # Forgets a persistent session.
   def forget(user)
-    user.forget
-    cookies.delete(:user_id)
+    user.forget #gọi đến hàm forget trong model update remember_digest bằng nil(xóa remember_digest)
+    cookies.delete(:user_id) 
     cookies.delete(:remember_token)
   end
 
   # Logs out the current user.
   def log_out
-    forget(current_user)
-    session.delete(:user_id)
+    forget(current_user) #xóa cookies
+    session.delete(:user_id) 
     @current_user = nil
   end
 
