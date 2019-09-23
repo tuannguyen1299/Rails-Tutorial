4f5be1ca403de684bce4483a4ad0e9102f285b34
tuannguyen1299 <tuannguyennst99@gmail.com>
2019-09-23 16:37:24 +0700

    chap14

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  default_scope -> { order(created_at: :ASC) }
+  default_scope -> { order(created_at: :DESC) }
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
